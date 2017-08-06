05aeb6dd60a174f73697804124f5a7e18dcd6655
Isaac <ilebwohlsteiner@gmail.com>
2017-08-06 00:59:43 -0400

    Fix quotes

--- wp-content/themes/Divi-ECRS/page-class-descriptions.php
+++ wp-content/themes/Divi-ECRS/page-class-descriptions.php
@@ -54,7 +54,7 @@ get_header();
   					    echo '<div class="class-names>';
   					      echo '<ul class="class-names">';
         					  foreach ($classes as $index=>$class) {
-        					    echo '<li class="class-name" onclick="location.hash=desc-' . $index .'">'
+        					    echo '<li class="class-name" onclick="location.hash=''desc-' . $index .'''>'
         					            . $class->post_title .
         					         '</li>';
         					  }
