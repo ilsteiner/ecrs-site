8f05ab6f38e01b435418f69a28a35af141930af8
Isaac <ilebwohlsteiner@gmail.com>
2017-08-21 01:41:21 -0400

    Make it update

--- wp-content/themes/Divi-ECRS/js/countdown.js
+++ wp-content/themes/Divi-ECRS/js/countdown.js
@@ -1,9 +1,17 @@
 jQuery(document).ready(function($) {
 	var softCutoff = jQuery("div.c-calculation-date[data-field='SoftCutoff'] .c-content").text();
 	
-	var counter = countdown(null,new Date(softCutoff),null,3);
+  // Add DOM element
+	jQuery(".c-calculation-singleline:first-of-type .c-content").append("<div class='counter'</div>");
 	
-	jQuery(".c-calculation-singleline:first-of-type .c-content").append("<div>"
-	  + counter.toString()
-	  + "</div>");
+	var counter =
+  countdown(
+  	function(ts) {
+      jQuery('.counter').html(ts.toHTML());
+    },
+    new Date(softCutoff),
+    countdown.DAYS |
+    countdown.HOURS |
+    countdown.MINUTES |
+    countdown.SECONDS,4);
 });
