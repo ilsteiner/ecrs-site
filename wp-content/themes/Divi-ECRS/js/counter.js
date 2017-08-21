4530e5fbfa4142dbc83ea215541d98673e3371c0
Isaac <ilebwohlsteiner@gmail.com>
2017-08-21 13:11:11 -0400

    Put everything in a function

--- wp-content/themes/Divi-ECRS/js/counter.js
+++ wp-content/themes/Divi-ECRS/js/counter.js
@@ -1,18 +1,24 @@
 jQuery(document).ready(function($) {
-	var softCutoff = jQuery("div.c-calculation-date[data-field='SoftCutoff'] .c-content").text();
-	
-	//Insert ordinal
-	addOrdinal(jQuery(".c-calculation-singleline:first-of-type .c-content"));
-	addOrdinal(jQuery(".c-calculation-singleline:last-of-type .c-content"));
-	
+  doTheStuff();
+});
+
+function doTheStuff() {
+  console.log("It fired!");
+  
+  var softCutoff = jQuery("div.c-calculation-date[data-field='SoftCutoff'] .c-content").text();
+  
+  //Insert ordinal
+  addOrdinal(jQuery(".c-calculation-singleline:first-of-type .c-content"));
+  addOrdinal(jQuery(".c-calculation-singleline:last-of-type .c-content"));
+  
   // Add DOM elements
-	jQuery(".c-calculation-singleline:first-of-type .c-content").append("<div class='counter'</div>");
-	jQuery(".c-calculation-singleline:last-of-type .c-content").append("<div class='counter'</div>");
-	
-	var counter =
+  jQuery(".c-calculation-singleline:first-of-type .c-content").append("<div class='counter'</div>");
+  jQuery(".c-calculation-singleline:last-of-type .c-content").append("<div class='counter'</div>");
+  
+  var counter =
   countdown(
-  	function(ts) {
-      console.log("It works with logging for some reason.");
+    function(ts) {
+      console.log("Counter fired!");
       jQuery('.counter').html(ts.toHTML());
     },
     new Date(softCutoff),
@@ -20,15 +26,17 @@ jQuery(document).ready(function($) {
     countdown.HOURS |
     countdown.MINUTES |
     countdown.SECONDS,4);
-});
+}
 
 function addOrdinal(element) {
+  console.log("Add ordinal");
   var num = element.text().match(/[\d]+/);
   var ordinalNum = getOrdinal(num);
   element.text(element.text().replace(/[\d]+/,ordinalNum));
 }
 
 function getOrdinal(n) {
+  console.log("Get ordinal");
     var s=["th","st","nd","rd"],
     v=n%100;
     return n+(s[(v-20)%10]||s[v]||s[0]);
