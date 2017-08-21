e43c7d95a4a1beba85bc236aa9e4f7ed3c923127
Isaac <ilebwohlsteiner@gmail.com>
2017-08-21 11:34:40 -0400

    Only load on events

--- wp-content/themes/Divi-ECRS/functions.php
+++ wp-content/themes/Divi-ECRS/functions.php
@@ -102,14 +102,18 @@ add_action( 'wp_enqueue_scripts', 'register_tabs_stuff' );
 
 //Enqueue registration stuff
 function register_registration_stuff() {
-  wp_enqueue_style( 'reg-styles', get_stylesheet_directory_uri() . '/css/registration.css' );
-  wp_enqueue_script( 'reg-script', get_stylesheet_directory_uri() . '/js/registration.js', array( 'jquery' ), '1.0', true );
+  if( is_page('single-event') ) {
+    wp_enqueue_style( 'reg-styles', get_stylesheet_directory_uri() . '/css/registration.css' );
+    wp_enqueue_script( 'reg-script', get_stylesheet_directory_uri() . '/js/registration.js', array( 'jquery' ), '1.0', true );
+  }
 }
 add_action( 'wp_enqueue_scripts', 'register_registration_stuff' );
 
 //Enqueue countdown stuff
 function register_countdown_stuff() {
-  wp_enqueue_script( 'count-script', get_stylesheet_directory_uri() . '/js/countdown.js', array( 'jquery' ), '1.0', true );
+  if( is_page('single-event') ) {
+   wp_enqueue_script( 'count-script', get_stylesheet_directory_uri() . '/js/countdown.js', array( 'jquery' ), '1.0', true );
+  }
 }
 add_action( 'wp_enqueue_scripts', 'register_countdown_stuff' );
 ?>
