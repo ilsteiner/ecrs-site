c2b273ed58c3b22e805456b7e85a5aa9bb61315b
Isaac <ilebwohlsteiner@gmail.com>
2017-08-21 00:59:03 -0400

    Mod max

--- wp-content/themes/Divi-ECRS/js/countdown.js
+++ wp-content/themes/Divi-ECRS/js/countdown.js
@@ -1,6 +1,9 @@
 jQuery(document).ready(function($) {
 	var softCutoff = jQuery("div.c-calculation-date[data-field='SoftCutoff'] .c-content").text();
+	
+	var counter = countdown(null,new Date(softCutoff),null,3);
+	
 	jQuery(".c-calculation-singleline:first-of-type .c-content").append("<div>"
-	  + countdown(new Date(softCutoff)).toString()
+	  + counter.toString()
 	  + "</div>");
 });
