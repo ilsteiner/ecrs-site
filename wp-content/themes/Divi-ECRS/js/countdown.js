8fcc818698f916dda345e78c3a53cdc6f61d3add
Isaac <ilebwohlsteiner@gmail.com>
2017-08-20 23:31:42 -0400

    Make jQuery work

--- wp-content/themes/Divi-ECRS/js/countdown.js
+++ wp-content/themes/Divi-ECRS/js/countdown.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function() {
+jQuery(document).ready(function($) {
 	jQuery(".c-calculation-singleline:first-of-type").prepend("<div class=\"clock\"></div>");
 
 	var clock = $('.clock').FlipClock(5619571, {
