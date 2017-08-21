5b79763b2576a3628b300e747f31ca508a43af93
Isaac <ilebwohlsteiner@gmail.com>
2017-08-20 23:22:27 -0400

    Add countdown

---
+++ wp-content/themes/Divi-ECRS/js/countdown.js
@@ -0,0 +1,8 @@
+$(function() {
+	jQuery(".c-calculation-singleline:first-of-type").prepend("<div class=\"clock\"></div>");
+
+	var clock = $('.clock').FlipClock(5619571, {
+			clockFace: 'DailyCounter',
+			countdown: true
+	});
+});
