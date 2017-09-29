98c3228a8547fb655192e30ffed7d43ab1ae287b
Isaac <ilebwohlsteiner@gmail.com>
2017-09-29 00:49:54 -0400

    Turn off the name cloning for registration since it breaks form submission

--- wp-content/themes/Divi-ECRS/js/registration.js
+++ wp-content/themes/Divi-ECRS/js/registration.js
@@ -1,6 +1,6 @@
 function reactToChange(selector,changing,changed) {
   jQuery(selector).eq(changing).on("change paste keyup", function() {
-    jQuery(selector).eq(changed).val(jQuery(this).val());
+    jQuery(selector).eq(changed).val(jQuery(this).val()).change();
   });
 
   //Make the changed field read only
@@ -10,11 +10,11 @@ function reactToChange(selector,changing,changed) {
 
 jQuery(document).ready(function(jQuery) {
 	// Changes to first name field affect third name field
-	reactToChange(".c-name input",0,2);
+	//reactToChange(".c-name input",0,2);
 
 	// Changes to second name field affect fourth name field
-	reactToChange(".c-name input",1,3);
+	//reactToChange(".c-name input",1,3);
 
 	//Changes to first email field affect second email field
-	reactToChange(".c-email input",0,1); 
+	//reactToChange(".c-email input",0,1); 
 });
